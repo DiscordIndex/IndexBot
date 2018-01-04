9c292289f914acd9ef7d05399eda649a040ed208
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-01-04 18:43:05 +0100

    adds missing file

---
+++ plugins/index/utils/discordindex.py
@@ -0,0 +1,150 @@
+from disco.api.http import APIException
+from pony import orm
+
+
+@orm.db_session
+def update_discord_index(plugin, only_in_channel_id=None):
+    index_guild = plugin.client.state.guilds.get(plugin.config.indexGuildID)
+    index_targets = {}
+
+    only_in_channel = None
+    if only_in_channel_id:
+        only_in_channel = index_guild.channels.get(only_in_channel_id)
+        parent_name = ""
+        if only_in_channel.parent_id:
+            parent_name = '#' + index_guild.channels.get(
+                only_in_channel.parent_id).name + ' / '
+        plugin.log.info(
+            'discordindex: updating channel ' + parent_name + '#' + only_in_channel.name + ' (' + str(
+                only_in_channel.id) + ')')
+    else:
+        plugin.log.info('discordindex: updating all channels')
+
+    for discord_server in orm.select(ds for ds in plugin.db.DiscordServer if ds.state == 2):
+        index_channel = get_channel_for_name_and_category(plugin, discord_server.category_channel_name,
+                                                          discord_server.genre_category_name, guild=index_guild)
+        if index_channel:
+            if only_in_channel and index_channel.id != only_in_channel.id:
+                # print('skipped #' + index_channel.name)
+                continue
+            if index_channel in index_targets:
+                index_targets[index_channel].append(discord_server)
+            else:
+                index_targets[index_channel] = [discord_server]
+        else:
+            plugin.log.warn('no channel found for discord_server #' + discord_server.id)
+
+    # print(index_targets)
+
+    if only_in_channel:
+        if only_in_channel not in index_targets:
+            # delete unused channel messages
+            index_target_channel_messages = plugin.client.api.channels_messages_list(
+                only_in_channel.id,
+                after=1,
+                limit=100)
+            # TODO: handle more than 100 messages, state?
+            index_target_channel_messages_iter = iter(index_target_channel_messages)
+            for leftover_message in index_target_channel_messages_iter:
+                if leftover_message.author.id != plugin.state.me.id:
+                    continue
+                # delete all following messages
+                try:
+                    plugin.client.api.channels_messages_delete(only_in_channel.id,
+                                                               leftover_message.id)
+                except APIException:
+                    pass
+
+    for index_target_channel, index_target_discord_servers in index_targets.items():
+        # print("#" + index_guild.channels.get(
+        #    index_target_channel.parent_id).name + " / #" + index_target_channel.name)
+        index_target_discord_servers.sort(
+            key=lambda discord_server: discord_server_sort_key(discord_server))
+
+        index_target_channel_messages = plugin.client.api.channels_messages_list(
+            index_target_channel.id,
+            after=1,
+            limit=100)
+        index_target_channel_messages.reverse()
+        # TODO: handle more than 100 messages, state?
+        index_target_channel_messages_iter = iter(index_target_channel_messages)
+
+        for index_target_channel_discord_server in index_target_discord_servers:
+            # print(index_target_channel_discord_server.name + " (" + cleanup_server_name_for_sorting(
+            #    index_target_channel_discord_server.name) + ")")
+            try:
+                while True:
+                    current_message = next(index_target_channel_messages_iter)
+                    if current_message.author.id != plugin.state.me.id:
+                        continue
+                    break
+            except StopIteration:
+                current_message = None
+
+            if current_message:
+                # update
+                if current_message.content != get_discord_server_message(
+                        index_target_channel_discord_server):
+                    # edit message
+                    plugin.client.api.channels_messages_modify(index_target_channel.id,
+                                                               current_message.id,
+                                                               get_discord_server_message(
+                                                                   index_target_channel_discord_server))
+            else:
+                # create
+                plugin.client.api.channels_messages_create(index_target_channel.id,
+                                                           get_discord_server_message(
+                                                               index_target_channel_discord_server))
+
+        for leftover_message in index_target_channel_messages_iter:
+            if leftover_message.author.id != plugin.state.me.id:
+                continue
+            # delete all following messages
+            try:
+                plugin.client.api.channels_messages_delete(index_target_channel.id,
+                                                           leftover_message.id)
+            except APIException:
+                pass
+
+
+def get_channel_for_name_and_category(plugin, channel_name, category_name, guild=None):
+    if not guild:
+        guild = plugin.client.state.guilds.get(plugin.config.indexGuildID)
+
+    for _, index_channel in guild.channels.items():
+        if index_channel.parent_id:
+            if category_name.lower() != guild.channels.get(
+                    index_channel.parent_id).name.lower():
+                continue
+        else:
+            if category_name:
+                continue
+        if channel_name.lower() != index_channel.name.lower():
+            continue
+        if index_channel.id in plugin.config.addChannelIDs:
+            continue
+        if index_channel.id in plugin.config.changelogChannelIDs:
+            continue
+        if index_channel.id == plugin.config.approvalQueueChannelID:
+            continue
+        return index_channel
+    return None
+
+
+def discord_server_sort_key(discord_server):
+    name = discord_server.name.lower()
+    if name.startswith('the '):
+        name = name[4:]
+    if name.startswith('a '):
+        name = name[2:]
+    name = ''.join(e for e in name if e.isalnum())
+    name = name.strip()
+    name += str(discord_server.submitted_at.timestamp())
+    # cleaned name + timestamp
+    return name
+
+
+def get_discord_server_message(discord_server):
+    return '**{discord_server.name}** https://discord.gg/{discord_server.invite_code}\n' \
+           '{discord_server.description}'.format(
+        discord_server=discord_server).strip()  # + ' (' + discord_server_sort_key(discord_server) + ')'
